83a6f63026f2cc852747bc25afe7dec139d5734d
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-11-12 22:40:29 -0300

    Melhorando UI da Home e adicionando novos recursos.

--- src/app/componentes/card-transacoes/card-transacoes.component.ts
+++ src/app/componentes/card-transacoes/card-transacoes.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { DADOS } from 'src/app/mock-transacoes';
-import {MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-card-transacoes',
   templateUrl: './card-transacoes.component.html',
