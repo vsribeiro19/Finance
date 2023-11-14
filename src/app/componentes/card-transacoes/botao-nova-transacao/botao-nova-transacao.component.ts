bfb15f6fcac07ad8c152247f56f10c8903bbe293
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-11-14 07:36:59 -0300

    Criando componente para botão de nova transação.

---
+++ src/app/componentes/card-transacoes/botao-nova-transacao/botao-nova-transacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-nova-transacao',
+  templateUrl: './botao-nova-transacao.component.html',
+  styleUrls: ['./botao-nova-transacao.component.scss']
+})
+export class BotaoNovaTransacaoComponent {
+
+}
