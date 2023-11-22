12adf4a9093cc5d8bc3d5e34e6b65ea1aa9c4f35
Victor Ribeiro <victor.ribeiro@comlink.com.br>
2023-11-22 16:47:17 -0300

    Ajustando nome do atributo nomeCompra DE: compra PARA: nomeCompra

--- src/app/mock-transacoes.ts
+++ src/app/mock-transacoes.ts
@@ -5,21 +5,21 @@ import { transacoes } from "./models/transacoes.model";
 export const DADOS: transacoes[] = [
     {
         idTransacao: 1,
-        compra: 'Plano Claro',
+        nomeCompra: 'Plano Claro',
         tipoCompra: TipoCompra.debito,
         status: StatusCompra.pendente,
         valor: 39.90
     },
     {
         idTransacao: 2,
-        compra: 'Plano Claro',
+        nomeCompra: 'Plano Claro',
         tipoCompra: TipoCompra.credito,
         status: StatusCompra.pendente,
         valor: 52.59
     },
     {
         idTransacao: 3,
-        compra: 'Plano Claro',
+        nomeCompra: 'Plano Claro',
         tipoCompra: TipoCompra.credito,
         status: StatusCompra.pago,
         valor: 150.42
