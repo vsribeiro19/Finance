ef2255b3c114dd4b006d9595021d553ac24ab74c
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-11-29 22:45:03 -0300

    Adicionando legenda de sem transações.

--- src/app/componentes/card-transacoes/card-transacoes.component.ts
+++ src/app/componentes/card-transacoes/card-transacoes.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DADOS } from 'src/app/mock-transacoes';
 @Component({
   selector: 'app-card-transacoes',
   templateUrl: './card-transacoes.component.html',
   styleUrls: ['./card-transacoes.component.scss']
 })
-export class CardTransacoesComponent {
+export class CardTransacoesComponent implements OnInit {
   dadosTransacao = DADOS;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+
+  
+  
 }
