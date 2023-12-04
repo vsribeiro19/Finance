3b287e032c52e69a8a36aec84e72c80c6ddf8dbc
Victor Ribeiro <victor.ribeiro@comlink.com.br>
2023-12-04 09:07:57 -0300

    Adicionando Firestore e colletions na  service de transações

--- src/app/services/transacoes.service.ts
+++ src/app/services/transacoes.service.ts
@@ -1,40 +1,24 @@
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { Injectable } from '@angular/core';
 import { Transacoes } from '../models/transacoes.model';
-import { Observable } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { map } from 'rxjs';
-
+import {Firestore,collection,addDoc} from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class TransacoesService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private firestore: Firestore) { }
 
-  listarTodasTransacoes() {
-    return this.db.list('transacao').snapshotChanges().pipe(
-      map(changes => {
-        // return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-      })
-    )
-  }
-
-  listarTransacoesPorId(id: Transacoes) {
-
-  }
-
-  novaTransacao(transacao: Transacoes) {
-    this.db.list('transacao').push(transacao).then((result: any) => {
-
+  novaTransacao(t: any){
+    const transacoesIntance = collection(this.firestore, 'transacao');
+    addDoc(transacoesIntance, t.value).then(() => {
+      
+    }).catch((err)=>{
+      console.log(err);
+      
     })
   }
 
-  editarTransacao(id: Transacoes) {
-
-  }
-
-  excluirTransacao(id: Transacoes) {
-
-  }
+  
 }
