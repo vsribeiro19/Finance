d9027e05edf88354ca1aff95198fa43cb0a66726
Victor Ribeiro <v.leitteribeiro@gmail.com>
2024-05-11 15:27:30 -0300

    refactor: Retirar código desnecessário

--- src/app/componentes/card-faturas/card-faturas.component.ts
+++ src/app/componentes/card-faturas/card-faturas.component.ts
@@ -8,31 +8,9 @@ import { TransacoesService } from 'src/app/services/transacoes.service';
   styleUrls: ['./card-faturas.component.scss']
 })
 export class CardFaturasComponent implements OnInit {
-  qtdTransacoes = [];
-  transacoes?: Observable<Transacoes[]>;
-  total = 0;
-  constructor(private transacoesService: TransacoesService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    // this.transacoes = this.transacoesService.obterTransacoes();
-    // this.verificaQtdTransacoes();
+
   }
-
-  // verificaQtdTransacoes() {
-  //   this.transacoesService.obterTransacoes().subscribe((parameters: any) => {
-  //     this.qtdTransacoes = parameters;
-  //     this.obterSomaTransacoes(this.qtdTransacoes);
-  //   });
-  //   return this.qtdTransacoes;
-  // }
-
-
-  // somaTransacoes(total: any, item: any) {
-  //   return total + (item.valor);
-  // }
-
-  // obterSomaTransacoes(transacoes: any) {
-  //   console.log('teste', transacoes);
-  //   this.total = transacoes.reduce(this.somaTransacoes, 0);
-  // }
 }
