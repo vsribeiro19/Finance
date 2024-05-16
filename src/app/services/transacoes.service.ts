af73d42afacac2847f25e666c8246f633c720b05
Victor Ribeiro <victor.ribeiro@comlink.com.br>
2024-05-16 10:36:57 -0300

    feature: Adicionar método service para trazer transação pelo Id

--- src/app/services/transacoes.service.ts
+++ src/app/services/transacoes.service.ts
@@ -2,12 +2,26 @@ import { Injectable } from '@angular/core';
 import { Transacoes } from '../models/transacoes.model';
 import { Firestore, collection, addDoc, collectionData, DocumentReference } from '@angular/fire/firestore';
 import { Observable, catchError, from, map, tap } from 'rxjs';
+import { getDoc, getDocs } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class TransacoesService {
 
+  db = getFirestore();
+  ref = collection(this.db, 'transacoes');
+  snapShot = getDocs(this.ref);
+
   constructor(private firestore: Firestore) { }
+
+  obterTransacaoPeloId(idTransacao: string) {
+    this.snapShot.then((response) => {
+      response.forEach((r) => {
+        idTransacao = r.id;
+      })
+    })
+  }
 
   obterTransacoes(): Observable<any> {
     const transacoes = collection(this.firestore, 'transacoes');
